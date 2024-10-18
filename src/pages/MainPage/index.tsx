0423135b1e25d710e802f76fbd52a1e7a5bd3868
Shakirov Samat <53141697+ShakirovSamat@users.noreply.github.com>
2024-10-18 16:39:28 +0300

    feat: add main page

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import styles from './index.module.css'
+
+export const MainPage = () => {
+    return (
+        <>
+            <h2 className={styles.title}>Методы вычислений и вычислительный практикум</h2>
+            <div className={styles.container}>
+                <Link className={styles.link} to={'/nonlinear_equations'}>Нелинейные уравнения</Link>
+            </div>
+        </>
+    );
+};
